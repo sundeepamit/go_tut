aec834c943d3f77685e24995f6212c9d98dd905b
sundeepamit <sundeepamitmaurya>
2026-05-22 14:58:46 +0530

    Type conversion

--- ch1/hello.go
+++ ch1/hello.go
@@ -6,4 +6,14 @@ func main() {
 	// calls Println() function from a fmt package.
 	// fmt.Println("Hello World!!!")
 	fmt.Printf("Hello, %s!!!!\n", "world")
+
+	// Type conversion
+	var x int64 = 10
+	var y float64 = 30.12
+	// converting int to float
+	var sum1 float64 = float64(x) + y
+	var sum2 int64 = x + int64(y)
+	fmt.Println("Sum1 is ", sum1)
+	fmt.Println("Sum2 is ", sum2)
+
 }
